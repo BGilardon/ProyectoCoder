1e96fa783b26fa2f0c5cd560e732890f0e04a241
bautista-gilardon <bautistagilardon@gmail.com>
2022-06-14 23:02:12 -0300

    Primer Plantilla Piola

--- MVTApp/views.py
+++ MVTApp/views.py
@@ -73,18 +73,8 @@ def fam(request):
 
 
 def inicio(request):
-   Html='''
-   <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-   </head>
-   <body>
-      <h1>Pagina de inicio</h1>
-   </body>
-   </html>
-   '''
-   return HttpResponse(Html)
+   pass
+
+   return render(request, 'index.html')
 
 
